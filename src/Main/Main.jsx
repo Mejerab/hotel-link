b479ba81ef0fa65023a19746e2290e7b202d9ae4
Mejerab <omim3670@gmail.com>
2024-10-05 12:10:13 +0600

    last commit

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -4,7 +4,7 @@ import Footer from "../Shared/Footer";
 
 const Main = () => {
     return (
-        <div className="open-sans bg-white">
+        <div className="open-sans">
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
