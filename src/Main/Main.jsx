142e2c629eaeeb2e72941d784ee7b368d415a607
Mejerab <omim3670@gmail.com>
2024-10-05 11:53:36 +0600

    nothing

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -4,7 +4,7 @@ import Footer from "../Shared/Footer";
 
 const Main = () => {
     return (
-        <div className="open-sans">
+        <div className="open-sans bg-white">
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
